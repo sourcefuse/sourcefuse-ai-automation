97e05e38338c2324ac740f9d9b46e0138984108a
ankit0412 <ankit.aswal@sourcefuse.com>
2025-06-02 19:01:58 +0530

    Removed api key from config

--- config.py
+++ config.py
@@ -1,14 +1,4 @@
 # config.py
-# import os
-# from pydantic_settings import BaseSettings  # Updated import
-#
-# class Settings(BaseSettings):
-#     gemini_api_key: str = os.getenv("Gemini_API_Key", "AIzaSyBl4tr07rY2AvmKGhbvspwXaX9x430zlbA")
-#     base_url: str = "https://www.saucedemo.com/"
-#
-# settings = Settings()
-
-# config.py
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
